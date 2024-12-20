66c1b0c249c76770681621b30d6d65b413f2754d
Aung-Pyae-Sone8 <apyae8263@gmail.com>
2024-12-20 13:44:34 +0630

    chat-list-design

---
+++ src/components/list/chatList/ChatList.jsx
@@ -0,0 +1,90 @@
+import { useState } from "react"
+import "./chatList.css"
+
+const ChatList = () => {
+    const [addMode, setAddMode] = useState(false)
+
+    return (
+        <div className="chatList">
+            <div className="search">
+                <div className="searchBar">
+                    <img src="/search.png" alt="" />
+                    <input type="text" name="" id="" placeholder="Search" />
+                </div>
+                <img src={addMode ? "./minus.png" : "./plus.png" } className="add" alt="" onClick={() => setAddMode((prev) => !prev)} />
+            </div>
+            <div className="item">
+                <img src="./avatar.png" alt="" />
+                <div className="texts">
+                    <span>Siesar</span>
+                    <p>Hello</p>
+                </div>
+            </div>
+            <div className="item">
+                <img src="./avatar.png" alt="" />
+                <div className="texts">
+                    <span>Ye Htet San</span>
+                    <p>Hello</p>
+                </div>
+            </div>
+            <div className="item">
+                <img src="./avatar.png" alt="" />
+                <div className="texts">
+                    <span>Nyi Htet Aung</span>
+                    <p>Hello</p>
+                </div>
+            </div>
+            <div className="item">
+                <img src="./avatar.png" alt="" />
+                <div className="texts">
+                    <span>Siesar</span>
+                    <p>Hello</p>
+                </div>
+            </div>
+            <div className="item">
+                <img src="./avatar.png" alt="" />
+                <div className="texts">
+                    <span>Ye Htet San</span>
+                    <p>Hello</p>
+                </div>
+            </div>
+            <div className="item">
+                <img src="./avatar.png" alt="" />
+                <div className="texts">
+                    <span>Nyi Htet Aung</span>
+                    <p>Hello</p>
+                </div>
+            </div>
+            <div className="item">
+                <img src="./avatar.png" alt="" />
+                <div className="texts">
+                    <span>Nyi Htet Aung</span>
+                    <p>Hello</p>
+                </div>
+            </div>
+            <div className="item">
+                <img src="./avatar.png" alt="" />
+                <div className="texts">
+                    <span>Siesar</span>
+                    <p>Hello</p>
+                </div>
+            </div>
+            <div className="item">
+                <img src="./avatar.png" alt="" />
+                <div className="texts">
+                    <span>Ye Htet San</span>
+                    <p>Hello</p>
+                </div>
+            </div>
+            <div className="item">
+                <img src="./avatar.png" alt="" />
+                <div className="texts">
+                    <span>Nyi Htet Aung</span>
+                    <p>Hello</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ChatList
